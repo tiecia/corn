fd4713fd2cb6fdd5c9e5ce105b3facae9eb7c98d
Tyler CH <tyler.ciahaz@outlook.com>
2023-07-30 19:31:27 -0700

    start corn api

---
+++ CornBot/CornAPI.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CornBot {
+    public class CornAPI {
+        public CornAPI() {
+
+        }
+
+        public async Task RunAsync() {
+            var builder = WebApplication.CreateBuilder();
+
+            // Add services to the container.
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment()) {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            await app.RunAsync();
+        }
+    }
+}
--- CornBot/Program.cs
+++ CornBot/Program.cs
@@ -5,5 +5,8 @@ using CornBot.Utilities;
 SimpleRNG.SetSeedFromSystemTime();
 
 var client = new CornClient();
+var api = new CornAPI();
 
+//await api.RunAsync();
 await client.MainAsync();
+
