7224135f587eaf73313992d8d474b3891af9143e
Tyler CH <tyler.ciahaz@outlook.com>
2023-07-31 19:56:48 -0700

    consolidate into /shuckerinfo endpoint

--- CornBot/CornAPI.cs
+++ CornBot/CornAPI.cs
@@ -1,15 +1,9 @@
 ﻿using CornBot.Models;
-using Discord;
-using Discord.Rest;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CornBot {
     public class CornAPI {
@@ -37,90 +31,117 @@ namespace CornBot {
 
             app.UseHttpsRedirection();
 
-
-
-            // Gets if a user has done all their dailies
-            // GET http://{baseurl}/shuckstatus?user={username}
-            app.MapGet("/shuckstatus", (HttpContext context) =>
-            {
-                string? queryUser = context.Request.Query["user"];
-                if(queryUser == null)
-                {
-                    context.Response.StatusCode = 400;
-                    return false;
-                }
-                var economy = _services.GetRequiredService<GuildTracker>();
-                int dailyCount = 0;
-                foreach (var guild in economy.Guilds.Values)
-                {
-                    foreach (var user in guild.Users.Values)
-                    {
-                        if(user.Username == queryUser && !user.HasClaimedDaily)
-                        {
-                            return false;
-                        } else if(user.Username == queryUser && user.HasClaimedDaily)
-                        {
-                            dailyCount++;
-                        }
-                    }
-                }
-
-                if(dailyCount == economy.Guilds.Count()) {
-                    return true;
-                } else
-                {
-                    return false;
-                }
-            });
-
-            // Gets the users total corn in a guild, or all guilds if no guild is given
+            // Gets either global or guild specific info for a user.
             // GET http://{baseurl}/corncount?user={username}
             // GET http://{baseurl}/shuckstatus?user={username}&guild={guildid}
-            app.MapGet("/corncount", (HttpContext context) =>
+            /* Example Response:
+            {
+                "Username": "tiec",
+                "ShuckStatus": false,
+                "CornCount": 15
+            }
+             */
+            app.MapGet("/shuckerinfo", (HttpContext context) =>
             {
                 string? queryUser = context.Request.Query["user"];
                 string? queryGuild = context.Request.Query["guild"];
 
+                context.Response.ContentType = "application/json";
+
                 if(queryUser == null)
                 {
                     context.Response.StatusCode = 400;
-                    return 0;
+                    return JsonConvert.SerializeObject(new ErrorResponse()
+                    {
+                        Error = "No user provided in query string."
+                    });
                 }
 
-                var economy = _services.GetRequiredService<GuildTracker>();
-                long cornCount = 0;
-                if(queryGuild == null)
+                return JsonConvert.SerializeObject(new ShuckerInfoResponse()
                 {
-                    foreach (var guild in economy.Guilds.Values)
-                    {
-                        foreach (var user in guild.Users.Values)
-                        {
-                            if(user.Username == queryUser)
-                            {
-                                cornCount += user.CornCount;
-                            }
-                        }
-                    }
-                } else
-                {
-                    foreach (var guild in economy.Guilds.Values)
-                    {
-                        if(guild.GuildId == ulong.Parse(queryGuild))
-                        {
-                            foreach (var user in guild.Users.Values)
-                            {
-                                if (user.Username == queryUser)
-                                {
-                                    return user.CornCount;
-                                }
-                            }
-                        }
-                    }
-                }
-                return cornCount;
+                    Username = queryUser,
+                    CornCount = GetCornCount(queryUser, queryGuild),
+                    ShuckStatus = GetShuckStatus(queryUser)
+                });
             });
 
             await app.RunAsync();
+        }
+
+        private bool GetShuckStatus(string queryUser)
+        {
+            var economy = _services.GetRequiredService<GuildTracker>();
+            int dailyCount = 0;
+            foreach (var guild in economy.Guilds.Values)
+            {
+                foreach (var user in guild.Users.Values)
+                {
+                    if (user.Username == queryUser && !user.HasClaimedDaily)
+                    {
+                        return false;
+                    }
+                    else if (user.Username == queryUser && user.HasClaimedDaily)
+                    {
+                        dailyCount++;
+                    }
+                }
+            }
+
+            if (dailyCount == economy.Guilds.Count())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private long GetCornCount(string queryUser, string? queryGuild)
+        {
+            var economy = _services.GetRequiredService<GuildTracker>();
+            long cornCount = 0;
+            if (queryGuild == null)
+            {
+                foreach (var guild in economy.Guilds.Values)
+                {
+                    foreach (var user in guild.Users.Values)
+                    {
+                        if (user.Username == queryUser)
+                        {
+                            cornCount += user.CornCount;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (var guild in economy.Guilds.Values)
+                {
+                    if (guild.GuildId == ulong.Parse(queryGuild))
+                    {
+                        foreach (var user in guild.Users.Values)
+                        {
+                            if (user.Username == queryUser)
+                            {
+                                return user.CornCount;
+                            }
+                        }
+                    }
+                }
+            }
+            return cornCount;
+        }
+        private class ShuckerInfoResponse
+        {
+            public string Username { get; set; } = "";
+            public bool ShuckStatus { get; set; }
+            public long CornCount { get; set; }
+        }
+
+        private class ErrorResponse
+        {
+            public string Error { get; set; } = "";
         }
     }
 }
